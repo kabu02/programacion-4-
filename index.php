607da66a8995d281c5f0e37b5209831c7644f4c1
kabu02 <66042097+kabu02@users.noreply.github.com>
2020-06-05 13:30:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="css/control.css">
+    </head>
+   <body>
+     <form action="conexion.php" method="post">
+      <div class="login-box">
+          <img class="avatar" src="imag/adm.png" alt="logo ingreso">
+          <h1>bienvenido </h1>
+          <form>
+                Nombre: <input type="text" name="nombre"><br>
+              
+
+             Email: <input type="text" name="email"><br>
+             
+
+             <input type="submit" class="btn" name="registrarse" value="Ingresar">
+
+           
+          </form>
+      </div>
+   </body>
+
+
+</html>
